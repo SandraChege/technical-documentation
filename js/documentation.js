abc56bfdca38d9da60a33ac1fa075a7ae3a811bf
sandra <chegemwihaki88@gmail.com>
2022-07-29 22:04:02 +0300

    add media queries

---
+++ js/documentation.js
@@ -0,0 +1,8 @@
+/*const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.nav-link').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+})*/
+
